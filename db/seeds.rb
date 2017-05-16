163bc7383d68f0319508d327acc1a998d9e28a53
Leland Krych <Leland.Krych@gmail.com>
2017-05-16 10:24:48 -0700

    Create AJAX calls

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+
+users = User.create([{email: 'user1@example.com', password: 'password'},
+                    {email: 'user2@example.com', password: 'password'}])
