9b325f791b1a6a9f0766f02227e55b0182bd5179
Leland Krych <Leland.Krych@gmail.com>
2017-05-16 10:10:42 -0700

    Add methods to users controller, and sessions controller

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,7 +1,20 @@
 class Api::SessionsController < ApplicationController
   def create
+    @user = User.find_by_credentials(user_params)
+    if @user
+      login(@user)
+      render "api/users/show"
+    else
+      render json: ["Invalid credentials"], status: 401
+    end
   end
 
   def destroy
+    if logged_in?
+      logout
+      render json: {}
+    else
+      render json: ["No logged in user"], status: 404
+    end
   end
 end
