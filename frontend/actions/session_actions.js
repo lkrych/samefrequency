6e3ff9fcd221bcf72e2267ea6b8f541c00b583e4
Leland Krych <Leland.Krych@gmail.com>
2017-05-16 10:41:32 -0700

    Create Session thunk action creators and action creators

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,33 @@
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
+export const RECEIVE_ERRORS = "RECEIVE_ERRORS";
+
+import * as APIUtil from '../util/session_api_util';
+
+const receiveCurrentUser = user => (
+  {
+    type: RECEIVE_CURRENT_USER,
+    user
+  }
+);
+
+const receiveErrors = errors => (
+  {
+    type: RECEIVE_ERRORS,
+    errors
+  }
+);
+
+export const signup = (user) => dispatch => (
+  APIUtil.signup(user).then(currentUser => receiveCurrentUser(currentUser)),
+  errors => receiveErrors(errors.responseJSON)
+);
+
+export const login = (user) => dispatch => (
+  APIUtil.login(user).then(currentUser => receiveCurrentUser(currentUser)),
+  errors => receiveErrors(errors.responseJSON)
+);
+
+export const logout = () => dispatch => (
+  APIUtil.logout().then(() => receiveCurrentUser()),
+  errors => receiveErrors(errors.responseJSON)
+);
--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import * as APIUtil from './util/session_api_util';
+
 document.addEventListener('DOMContentLoaded', () => {
     const root = document.getElementById('root');
     ReactDOM.render(<h1>Welcome to Same Frequency</h1>, root);
 });
+
+window.APIUtil = APIUtil;
