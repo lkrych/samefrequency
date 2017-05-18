f2ce22fa3398ea9aa3bf3deb173e6aec4b1e0cc0
Leland Krych <Leland.Krych@gmail.com>
2017-05-18 14:51:26 -0700

    Add limit to search query, give hover style to info, last save before major refactoring in StationsView and StationsDetail

--- app/controllers/api/stations_controller.rb
+++ app/controllers/api/stations_controller.rb
@@ -6,14 +6,14 @@ class Api::StationsController < ApplicationController
 
   def search
     search_term = URI.encode(params[:searchTerm])
-    response = HTTParty.get("http://api.shoutcast.com/legacy/stationsearch?k=#{Figaro.env.SHOUTCAST_KEY}&search=#{search_term}")
+    response = HTTParty.get("http://api.shoutcast.com/legacy/stationsearch?k=#{Figaro.env.SHOUTCAST_KEY}&limit=16&search=#{search_term}")
     render :json => clean_response(response).to_json
   end
 
   private
 
   def clean_response(response)
-    clean = {} 
+    clean = {}
     necessary_keys = ["name", "id", "genre"]
     station_info = response.parsed_response["stationlist"]["station"]
     station_info.each do |station|
