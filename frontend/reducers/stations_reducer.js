f8660261cd39b887c588572a39b86f947b9737d7
Leland Krych <Leland.Krych@gmail.com>
2017-05-17 17:51:11 -0700

    Add errors to stations

--- frontend/reducers/stations_reducer.js
+++ frontend/reducers/stations_reducer.js
@@ -1,10 +1,19 @@
 import { RECEIVE_STATIONS } from '../actions/stations_actions';
+import { RECEIVE_ERRORS } from '../actions/error_actions';
+import merge from 'lodash/merge';
 
-const stationsReducer = (state = {}, action) => {
+const _nullStations= {
+  stations: {},
+  errors: []
+};
+
+const stationsReducer = (state = _nullStations, action) => {
   Object.freeze(state);
   switch(action.type){
     case RECEIVE_STATIONS:
-      return action.stations;
+      return merge({}, _nullStations, {stations: action.stations});
+    case RECEIVE_ERRORS:
+      return merge({}, _nullStations, {errors: action.errors});
     default:
       return state;
   }
