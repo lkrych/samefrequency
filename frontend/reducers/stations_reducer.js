d848cf5698497ad731a7a8008c6e9977e4076183
Leland Krych <Leland.Krych@gmail.com>
2017-05-17 17:45:11 -0700

    delete console log

--- frontend/reducers/stations_reducer.js
+++ frontend/reducers/stations_reducer.js
@@ -1,6 +1,6 @@
-import RECEIVE_STATIONS from '../actions/stations_actions';
+import { RECEIVE_STATIONS } from '../actions/stations_actions';
 
-const stationsReducer = (state = [], action) => {
+const stationsReducer = (state = {}, action) => {
   Object.freeze(state);
   switch(action.type){
     case RECEIVE_STATIONS:
