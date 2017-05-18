ed88873586172cf60bcb61779af06a8f844856b3
Leland Krych <Leland.Krych@gmail.com>
2017-05-17 17:15:08 -0700

    refactor stations_controller

--- app/controllers/api/stations_controller.rb
+++ app/controllers/api/stations_controller.rb
@@ -1,24 +1,24 @@
 class Api::StationsController < ApplicationController
   def index
     response = HTTParty.get("http://api.shoutcast.com/legacy/Top500?k=#{Figaro.env.SHOUTCAST_KEY}&limit=16")
-    return clean_response(response)
+    render :json => clean_response(response).to_json
   end
 
   def search
     search_term = URI.encode(params[:searchTerm])
     response = HTTParty.get("http://api.shoutcast.com/legacy/stationsearch?k=#{Figaro.env.SHOUTCAST_KEY}&search=#{search_term}")
-    return clean_response(response)
+    render :json => clean_response(response).to_json
   end
 
   private
 
   def clean_response(response)
-    clean = []
+    clean = {} 
     necessary_keys = ["name", "id", "genre"]
     station_info = response.parsed_response["stationlist"]["station"]
     station_info.each do |station|
       cleaned = station.select {|key| necessary_keys.include?(key)}
-      clean.push(cleaned.to_json)
+      clean[cleaned["id"].to_i] = cleaned
     end
     return clean
   end
