4020295a26a2a282e9d702157f3c6299f55647fa
Leland Krych <Leland.Krych@gmail.com>
2017-05-19 13:38:59 -0700

    Create reducer for streams

--- frontend/reducers/root_reducer.js
+++ frontend/reducers/root_reducer.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 
 import sessionReducer from './session_reducer';
 import stationsReducer from './stations_reducer';
+import streamReducer from './stream_reducer';
 
  const rootReducer = combineReducers({
   session: sessionReducer,
-  stations: stationsReducer
+  stations: stationsReducer,
+  stream: streamReducer
 });
 
 export default rootReducer;
---
+++ frontend/reducers/stream_reducer.js
@@ -0,0 +1,22 @@
+import { RECEIVE_STREAM_URI} from '../actions/stream_actions';
+import { RECEIVE_ERRORS } from '../actions/error_actions';
+import merge from 'lodash/merge';
+
+const _nullStream= {
+  streamUri: {},
+  errors: []
+};
+
+const streamReducer = (state = _nullStream, action) => {
+  Object.freeze(state);
+  switch(action.type){
+    case RECEIVE_STREAM_URI:
+      return merge({}, _nullStream, {streamUri: action.uri});
+    case RECEIVE_ERRORS:
+      return merge({}, _nullStream, {errors: action.errors});
+    default:
+      return state;
+  }
+};
+
+export default streamReducer;
