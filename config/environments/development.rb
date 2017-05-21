39946bab9bd73e9d0bd9a5b3e7a760a471431532
Leland Krych <Leland.Krych@gmail.com>
2017-05-21 16:15:43 -0700

    add heroku config settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,5 +53,7 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   #tells the consumer where to listen for action cable updates
-  config.action_cable.url = "ws://localhost:3000/cable"
+  config.action_cable.url = "wss://still-taiga-52460.herokuapp.com//cable"
+  #tell the actioncable server where to accept requests
+  config.action_cable.allowed_request_origins = ['https://still-taiga-52460.herokuapp.com', 'http://still-taiga-52460.herokuapp.com']
 end
