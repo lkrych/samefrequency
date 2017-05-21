3c720d08625b776dc75aec6b9d5e0bdf9ca30802
Leland Krych <Leland.Krych@gmail.com>
2017-05-20 22:00:16 -0700

    rewrite Chatroom model to use station_id, create messages controller and chatrooms controller

---
+++ app/controllers/chatrooms_controller.rb
@@ -0,0 +1,7 @@
+class ChatroomsController < ApplicationController
+
+  def show
+    @chatroom = Chatroom.find_by(station_id: params[:station_id])
+    @message = Message.new
+  end
+end
--- db/migrate/20170521045314_create_chatrooms.rb
+++ db/migrate/20170521045314_create_chatrooms.rb
@@ -1,7 +1,7 @@
 class CreateChatrooms < ActiveRecord::Migration[5.0]
   def change
     create_table :chatrooms do |t|
-      t.string :station_name
+      t.integer :station_id
       t.timestamps
     end
   end
