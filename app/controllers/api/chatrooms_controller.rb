75425b277b7a94ffdb58f9312791af2e41ea6eb8
Leland Krych <Leland.Krych@gmail.com>
2017-05-22 09:37:55 -0700

    Add boilerplate to chat connection

--- app/controllers/api/chatrooms_controller.rb
+++ app/controllers/api/chatrooms_controller.rb
@@ -1,6 +1,6 @@
 class Api::ChatroomsController < ApplicationController
   def show
     @chatroom = Chatroom.find_by(station_id: params[:station_id])
-    render :json => @chatroom.messages.last(10).to_json
+    render :json => @chatroom.messages.last(20).to_json
   end
 end
