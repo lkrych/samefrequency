880fad167d73fe560b51decd96cc238fff2f7986
Leland Krych <Leland.Krych@gmail.com>
2017-05-22 10:47:13 -0700

    Add callback functions to chat component

--- frontend/components/chat/chat.jsx
+++ frontend/components/chat/chat.jsx
@@ -4,14 +4,28 @@ import MessageDetail from './message_detail';
 class Chat extends React.Component {
   constructor(props){
     super(props);
+    this.state = {message: ''};
+    this.onInput = this.onInput.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
   componentDidMount(){
-    window.App.cable.subscriptions.create({ channel: "StationsChannel",
+    window.App.chatchannel = window.App.cable.subscriptions.create(
+      { channel: "StationsChannel",
       station_id: this.props.station.id }, {
       received: (data) => {
         this.props.receiveMessages(data.content);
       }
     });
+  }
+
+  onInput(e){
+    e.preventDefault();
+    this.setState( { message: e.target.value });
+  }
+
+  handleSubmit(){
+    window.App.chatchannel.send(
+      { content: this.state.message, chatroom_id: this.props.station.id });
   }
   render(){
     const messages = this.props.messages.map(message => (
@@ -23,7 +37,14 @@ class Chat extends React.Component {
           {messages}
         </ul>
 
-        <form className="chat-text-input">
+        <form className="chat-text-input" onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <input type='text'
+              className="form-control"
+              onChange={this.onInput}
+              value={this.state.email}>
+            </input>
+          </div>
         </form>
       </div>
     );
