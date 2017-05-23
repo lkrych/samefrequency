5906c7dac8ce9d8c4f9b64da533392e08a13958c
Leland Krych <Leland.Krych@gmail.com>
2017-05-23 10:20:23 -0700

    Add local time to comment

--- frontend/components/chat/message_detail.jsx
+++ frontend/components/chat/message_detail.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
 export default (props) => {
-
+  const time = new Date(props.message.updated_at);
   return(
     <li className="message-detail">
-      {props.message.content}
+      <h5 className="message-sender">{props.message.author.username}</h5>
+      <p className="message-time">{time.toLocaleTimeString()}</p>
+      <p className="message-content">{props.message.content}</p>
     </li>
   );
 };
