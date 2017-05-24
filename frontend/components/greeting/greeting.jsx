f47d4bc403b84255e723fde563299ca9b5ddcd1b
Leland Krych <Leland.Krych@gmail.com>
2017-05-24 13:57:29 -0700

    Add basic modal with styling on greeting container

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -1,15 +1,46 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Modal from 'react-modal';
 
-const Greeting = ({ session, logout }) => {
+import {style} from './style';
 
+class Greeting extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { modalOpen: false };
+    this.toggleModal = this.toggleModal.bind(this);
+    this.onModalClose = this.onModalClose.bind(this);
+  }
+
+  toggleModal(){
+    this.setState({modalOpen: true});
+  }
+
+  onModalClose(){
+    this.setState({modalOpen: false});
+  }
+
+  render(){
     return (
       <section>
-        <p className="navbar-text">Welcome, {session.currentUser.email}! </p>
-        <button onClick={logout} className ="btn btn-default navbar-btn ">Log out</button>
+        <p className="navbar-text navbar-user-info"
+          onClick={this.toggleModal}>
+          Welcome, {this.props.session.currentUser.email}! </p>
+
+        <button onClick={this.props.logout}
+          className ="btn btn-default navbar-btn ">Log out</button>
+
+        <Modal
+          isOpen={this.state.modalOpen}
+          onRequestClose={this.onModalClose}
+          style={style}
+          >
+          <button onClick={this.onModalClose}>Close</button>
+          ...content
+        </Modal>
       </section>
     );
-
-};
+  }
+}
 
 export default Greeting;
