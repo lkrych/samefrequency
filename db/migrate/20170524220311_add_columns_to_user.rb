748576bab2bc61621a645e8e8a17995cb2d06d6b
Leland Krych <Leland.Krych@gmail.com>
2017-05-24 15:25:31 -0700

    rewrite user to start with 128 pixel radio

--- db/migrate/20170524220311_add_columns_to_user.rb
+++ db/migrate/20170524220311_add_columns_to_user.rb
@@ -1,6 +1,6 @@
 class AddColumnsToUser < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :image_url, :string, default: "https://res.cloudinary.com/heab4q3lg/image/upload/v1495662591/radio.png"
+    add_column :users, :image_url, :string, default: "https://res.cloudinary.com/heab4q3lg/image/upload/h_128/v1495662591/radio.png"
     add_column :users, :username, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -45,12 +45,12 @@ ActiveRecord::Schema.define(version: 20170524220311) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                                                                                               null: false
-    t.string   "password_digest",                                                                                     null: false
-    t.string   "session_token",                                                                                       null: false
-    t.datetime "created_at",                                                                                          null: false
-    t.datetime "updated_at",                                                                                          null: false
-    t.string   "image_url",       default: "https://res.cloudinary.com/heab4q3lg/image/upload/v1495662591/radio.png"
+    t.string   "email",                                                                                                     null: false
+    t.string   "password_digest",                                                                                           null: false
+    t.string   "session_token",                                                                                             null: false
+    t.datetime "created_at",                                                                                                null: false
+    t.datetime "updated_at",                                                                                                null: false
+    t.string   "image_url",       default: "https://res.cloudinary.com/heab4q3lg/image/upload/h_128/v1495662591/radio.png"
     t.string   "username"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["session_token"], name: "index_users_on_session_token", unique: true, using: :btree
