83d6cc23255a950d091ae8f5a1397ff51fb545eb
Leland Krych <Leland.Krych@gmail.com>
2017-05-24 15:10:12 -0700

    change json builder and migrate user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170522044349) do
+ActiveRecord::Schema.define(version: 20170524220311) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,11 +45,13 @@ ActiveRecord::Schema.define(version: 20170522044349) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",           null: false
-    t.string   "password_digest", null: false
-    t.string   "session_token",   null: false
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.string   "email",                                                                                               null: false
+    t.string   "password_digest",                                                                                     null: false
+    t.string   "session_token",                                                                                       null: false
+    t.datetime "created_at",                                                                                          null: false
+    t.datetime "updated_at",                                                                                          null: false
+    t.string   "image_url",       default: "https://res.cloudinary.com/heab4q3lg/image/upload/v1495662591/radio.png"
+    t.string   "username"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["session_token"], name: "index_users_on_session_token", unique: true, using: :btree
   end
